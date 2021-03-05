bba8d772bc971966dc17edb88ab2a2a31e78092e
Paige Bowman <paigeelisabethbowman@gmail.com>
2021-03-05 10:34:52 -0600

    Initial Commit

---
+++ API Calling Project/API_Calling_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Calling_ProjectApp.swift
+//  API Calling Project
+//
+//  Created by Student on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct API_Calling_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
